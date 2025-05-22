cdedc4350734ac421e8da3919efcd6439921996c
Antoniofo <antonio57201@gmail.com>
2025-05-22 16:22:12 +0200

    TO TEST IF SOUND FIX

--- MapEditorReborn/Patches/UpdatePositionServerPatch.cs
+++ MapEditorReborn/Patches/UpdatePositionServerPatch.cs
@@ -17,9 +17,14 @@ namespace MapEditorReborn.Patches
     {
         private static bool Prefix(AdminToyBase __instance)
         {
-            __instance.NetworkPosition = __instance.transform.position;
-            __instance.NetworkRotation = __instance.transform.rotation;
-            __instance.NetworkScale = __instance.transform.root != __instance.transform ? Vector3.Scale(__instance.transform.localScale, __instance.transform.root.localScale) : __instance.transform.localScale;
+            if (!__instance.gameObject.TryGetComponent<SpeakerToy>(out SpeakerToy _) || !__instance.gameObject.TryGetComponent<Speaker>(out Speaker _))
+            {
+                __instance.NetworkPosition = __instance.transform.position;
+                __instance.NetworkRotation = __instance.transform.rotation;
+                __instance.NetworkScale = __instance.transform.root != __instance.transform
+                    ? Vector3.Scale(__instance.transform.localScale, __instance.transform.root.localScale)
+                    : __instance.transform.localScale;
+            }
 
             return false;
         }
